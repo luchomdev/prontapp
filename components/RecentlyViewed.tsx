2bdba3718c8e8ac64af121b0ec9bff80fe9cc7cd
Genner Puello Teran <webmaster@dsldatasolutions.net>
2024-11-08 19:59:12 -0500

    fix productos vistos recientemente que muestre un mensaje si no hay productos recientes

--- components/RecentlyViewed.tsx
+++ components/RecentlyViewed.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react';
-import RecentlyViewedProductCard from './RecentlyViewedProductCard';
+import RecentlyViewedProductCard from '@/components/RecentlyViewedProductCard';
+import NoData from '@/components/NoData';
 import { useStore } from '@/stores/cartStore';
 
 const RecentlyViewed: React.FC = () => {
@@ -8,24 +9,24 @@ const RecentlyViewed: React.FC = () => {
 
   const products = Object.values(recentlyViewedProducts);
 
-  if (products.length === 0) {
-    return null;
-  }
-
   return (
-    <div className="my-8 ">
+    <div className="my-8">
       <h2 className="text-xl font-bold mb-4">Productos vistos recientemente</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {products.map((product) => (
-          <RecentlyViewedProductCard
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            price={product.price}
-            thumbnail={product.thumbnail}
-          />
-        ))}
-      </div>
+      {products.length === 0 ? (
+        <NoData message="No has visto productos recientemente" />
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          {products.map((product) => (
+            <RecentlyViewedProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              thumbnail={product.thumbnail}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
